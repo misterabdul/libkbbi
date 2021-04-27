3b96a1b1aa945b45af1d98bba2bc6aa50c419daa
Abdul Rahmad Pasaribu <abdoelrachmad@gmail.com>
2021-04-27 12:17:03 +0700

    feat: try to integrate with the data source deps

--- src/libkbbi.c
+++ src/libkbbi.c
@@ -1,5 +1,23 @@
 #include "libkbbi.h"
 
+Results*
+init_result()
+{
+  return kbbi_data_init_result();
+}
+
+void
+free_result(Results* results)
+{
+  return kbbi_data_free_result(results);
+}
+
+void
+search(Results** results, const char* query, const int query_size)
+{
+  return kbbi_data_search(results, query, query_size);
+}
+
 char*
 test()
 {
--- src/libkbbi.h
+++ src/libkbbi.h
@@ -1,7 +1,18 @@
 #ifndef _LIB_KBBI_H
 #define _LIB_KBBI_H
 
-char*
+#include "kbbi_data.h"
+
+extern char*
 test();
+
+extern Results*
+init_result();
+
+extern void
+free_result(Results* result);
+
+extern void
+search(Results** result, const char* query, const int query_size);
 
 #endif
